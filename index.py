23d620eec03c4ccf6e85836e06265ec24c84018a
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-24 20:26:28 +0600

    added functions for syslw if

--- index.py
+++ index.py
@@ -3,3 +3,9 @@ from pytube import YouTube
 link = input("Paste a link from Youtube")
 video = YouTube(link)
 quality = input("Choose the quality of the video (High/Low: " )
+
+if quality == "High":
+  output = video.streams.get_highest_resolution()
+if quality =="Low":
+  output = video.streams.get_lowest_resolution()
+  
